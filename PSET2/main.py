13baef039a662a5542861925739ffc71cb434c45
APogue <anpogue@ucla.edu>
2018-12-11 02:11:20 -0800

    Update main.py

--- PSET2/main.py
+++ PSET2/main.py
@@ -11,6 +11,9 @@ Pe = .1
 # to only have the heading point downwards in the goal state, input 'not all'
 # this will not work with the initial policy, I didn't design it with that capability
 
+# may be an error due to python interpreter versions, need to quick fix using if statements
+case = 'initial policy' # initial policy, policy iteration, or other
+
 example = gridWorlds(6, 6, 12, Pe, 'all')
 
 possible_goal_states = example.goal('all')
@@ -28,91 +31,94 @@ REWARD_MATRIX[4, 3] = 1
 # create grid world object
 grid_world = gridWorld('6x6 grid', REWARD_MATRIX, possible_goal_states)
 
-# generate and plot a trajectory, initial policy
-state = (1, 4, 6)
-trajectory = [state]
-actionMatrix = example.policy_matrix()
-valuePi0 = example.policy_evaluation(actionMatrix)
-value = valuePi0[1][4][6]
-trajectory_value = [value]
-grid_world.updateState(state)
-grid_world.updateValue(value)
-while state not in possible_goal_states:
-    action = actionMatrix[state]
-    transition = example.transition_function(Pe, state, action) # with Pe = 0, it should get there in min moves
-    state = tuple(transition)
+if case == 'initial policy':
+    # generate and plot a trajectory, initial policy
+    state = (1, 4, 6)
+    trajectory = [state]
+    actionMatrix = example.policy_matrix()
+    valuePi0 = example.policy_evaluation(actionMatrix)
+    value = valuePi0[1][4][6]
+    trajectory_value = [value]
     grid_world.updateState(state)
-    trajectory.append(state)
-    x = state[0]
-    y = state[1]
-    h = state[2]
-    value = valuePi0[x][y][h]
     grid_world.updateValue(value)
-    trajectory_value.append(value)
-grid_world.plotTrajectoryGradient()
-grid_world.saveFigure('trajectory', 'Initial Policy Trajectory', '.pdf')
-grid_world.saveFigure('value', 'Iniital Policy Value', '.pdf')
-print trajectory
-print trajectory_value
+    while state not in possible_goal_states:
+        action = actionMatrix[state]
+        transition = example.transition_function(Pe, state, action) # with Pe = 0, it should get there in min moves
+        state = tuple(transition)
+        grid_world.updateState(state)
+        trajectory.append(state)
+        x = state[0]
+        y = state[1]
+        h = state[2]
+        value = valuePi0[x][y][h]
+        grid_world.updateValue(value)
+        trajectory_value.append(value)
+    grid_world.plotTrajectoryGradient()
+    grid_world.saveFigure('trajectory', 'Initial Policy Trajectory', '.pdf')
+    grid_world.saveFigure('value', 'Iniital Policy Value', '.pdf')
+    print trajectory
+    print trajectory_value
 
-# generate and plot a trajectory, policy iteration
-state = (1, 4, 6)
-trajectory = [state]
-actionMatrix0 = example.policy_matrix()
-optPolicyMatrix = example.policy_iteration(actionMatrix0)
-valuePiStar = example.policy_evaluation(optPolicyMatrix)
-value = valuePiStar[1][4][6]
-trajectory_value = [value]
-grid_world.updateState(state)
-grid_world.updateValue(value)
-while state not in possible_goal_states:
-    prev_value = value
-    action = optPolicyMatrix[state]
-    transition = example.transition_function(Pe, state, action)
-    state = tuple(transition)
+elif case == 'policy iteration':
+    # generate and plot a trajectory, policy iteration
+    state = (1, 4, 6)
+    trajectory = [state]
+    actionMatrix0 = example.policy_matrix()
+    optPolicyMatrix = example.policy_iteration(actionMatrix0)
+    valuePiStar = example.policy_evaluation(optPolicyMatrix)
+    value = valuePiStar[1][4][6]
+    trajectory_value = [value]
     grid_world.updateState(state)
-    trajectory.append(state)
-    x = state[0]
-    y = state[1]
-    h = state[2]
-    value = valuePiStar[x][y][h]
     grid_world.updateValue(value)
-    trajectory_value.append(value)
-    if prev_value == value:
-        break
-grid_world.plotTrajectoryGradient()
-grid_world.saveFigure('trajectory', 'PolicyOptimalTrajectoryGoals', '.pdf')
-grid_world.saveFigure('value', 'PolicyOptimalSelectGoals', '.pdf')
-print trajectory
-print trajectory_value
+    while state not in possible_goal_states:
+        prev_value = value
+        action = optPolicyMatrix[state]
+        transition = example.transition_function(Pe, state, action)
+        state = tuple(transition)
+        grid_world.updateState(state)
+        trajectory.append(state)
+        x = state[0]
+        y = state[1]
+        h = state[2]
+        value = valuePiStar[x][y][h]
+        grid_world.updateValue(value)
+        trajectory_value.append(value)
+        if prev_value == value:
+            break
+    grid_world.plotTrajectoryGradient()
+    grid_world.saveFigure('trajectory', 'PolicyOptimalTrajectoryGoals', '.pdf')
+    grid_world.saveFigure('value', 'PolicyOptimalSelectGoals', '.pdf')
+    print trajectory
+    print trajectory_value
 
-# generate and plot a trajectory, value iteration
-state = (1, 4, 6)
-trajectory = [state]
-optPolicyMatrix, valuePiStar = example.value_iteration()
-value = valuePiStar[1][4][6]
-trajectory_value = [value]
-grid_world.updateState(state)
-grid_world.updateValue(value)
-while state not in possible_goal_states:
-    prev_value = value
-    action = optPolicyMatrix[state]
-    transition = example.transition_function(Pe, state, action)
-    state = tuple(transition)
+else:
+    # generate and plot a trajectory, value iteration
+    state = (1, 4, 6)
+    trajectory = [state]
+    optPolicyMatrix, valuePiStar = example.value_iteration()
+    value = valuePiStar[1][4][6]
+    trajectory_value = [value]
     grid_world.updateState(state)
-    trajectory.append(state)
-    x = state[0]
-    y = state[1]
-    h = state[2]
-    value = valuePiStar[x][y][h]
     grid_world.updateValue(value)
-    trajectory_value.append(value)
-    if prev_value == value:
-        break
-grid_world.plotTrajectoryGradient()
-grid_world.saveFigure('trajectory', 'ValueIterationTrajectoryGoals', '.pdf')
-grid_world.saveFigure('value', 'ValueIterationGoals', '.pdf')
-print trajectory
-print trajectory_value
+    while state not in possible_goal_states:
+        prev_value = value
+        action = optPolicyMatrix[state]
+        transition = example.transition_function(Pe, state, action)
+        state = tuple(transition)
+        grid_world.updateState(state)
+        trajectory.append(state)
+        x = state[0]
+        y = state[1]
+        h = state[2]
+        value = valuePiStar[x][y][h]
+        grid_world.updateValue(value)
+        trajectory_value.append(value)
+        if prev_value == value:
+            break
+    grid_world.plotTrajectoryGradient()
+    grid_world.saveFigure('trajectory', 'ValueIterationTrajectoryGoals', '.pdf')
+    grid_world.saveFigure('value', 'ValueIterationGoals', '.pdf')
+    print trajectory
+    print trajectory_value
 
 raw_input('Press Enter when finished')
